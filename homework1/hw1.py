33f3d46af079e37c428253289fdd3dff135efb09
Stacy Kemunto <kemuntos@vcu.edu>
2021-02-15 20:51:17 -0500

    changes.

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -16,7 +16,7 @@ def return_lowercased_string(input_string):
     Return it but the lowercase version of it."""
     return_value = input_string
     return return_value.lower()
-    
+
 def return_without_starting_ending_whitespace(input_string):
     """You have a variable called input_string that is of type string.
     Return it but with the surrounding (left and right) whitespace stripped."""
